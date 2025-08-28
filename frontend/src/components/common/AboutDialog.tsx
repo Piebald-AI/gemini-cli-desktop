9eff4f7adacf09e46975dafb480e4f027e6b6eed
bl-ue <54780737+bl-ue@users.noreply.github.com>
2025-08-27 20:27:11 -0600

    v0.2.0

--- frontend/src/components/common/AboutDialog.tsx
+++ frontend/src/components/common/AboutDialog.tsx
@@ -27,7 +27,7 @@ export const AboutDialog: React.FC<AboutDialogProps> = ({
   const backendText = getBackendText(selectedBackend);
 
   const appName = backendText.desktopName;
-  const appVersion = "0.1.0";
+  const appVersion = "0.2.0";
   const currentYear = new Date().getFullYear();
 
   return (
