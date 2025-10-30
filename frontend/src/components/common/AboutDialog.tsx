f9954e7ae4fdd395494e8d1985ddb787c734c252
signadou <39036502+signadou@users.noreply.github.com>
2025-10-29 20:09:02 -0600

    release 0.3.11 (#158)

--- frontend/src/components/common/AboutDialog.tsx
+++ frontend/src/components/common/AboutDialog.tsx
@@ -27,7 +27,7 @@ export const AboutDialog: React.FC<AboutDialogProps> = ({
   const backendText = getBackendText(selectedBackend);
 
   const appName = backendText.desktopName;
-  const appVersion = "0.3.10";
+  const appVersion = "0.3.11";
   const currentYear = new Date().getFullYear();
 
   return (
