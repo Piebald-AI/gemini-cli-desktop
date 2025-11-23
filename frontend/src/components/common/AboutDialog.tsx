e55db7af19d2a81d97546f4c1902d04531f80900
signadou <39036502+signadou@users.noreply.github.com>
2025-11-23 11:02:09 -0700

    Codesign the Windows executable (#166)

--- frontend/src/components/common/AboutDialog.tsx
+++ frontend/src/components/common/AboutDialog.tsx
@@ -27,7 +27,7 @@ export const AboutDialog: React.FC<AboutDialogProps> = ({
   const backendText = getBackendText(selectedBackend);
 
   const appName = backendText.desktopName;
-  const appVersion = "0.3.13";
+  const appVersion = "0.3.14";
   const currentYear = new Date().getFullYear();
 
   return (
