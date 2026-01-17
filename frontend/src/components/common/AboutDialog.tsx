15681393ce7552144034f48a44896a8a2202b610
George Parker <george@piebald.ai>
2026-01-17 10:02:17 -0700

    v0.3.15

--- frontend/src/components/common/AboutDialog.tsx
+++ frontend/src/components/common/AboutDialog.tsx
@@ -27,7 +27,7 @@ export const AboutDialog: React.FC<AboutDialogProps> = ({
   const backendText = getBackendText(selectedBackend);
 
   const appName = backendText.desktopName;
-  const appVersion = "0.3.14";
+  const appVersion = "0.3.15";
   const currentYear = new Date().getFullYear();
 
   return (
